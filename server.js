f3c9fd041b7c1528f7a96bef7f14f9cc91b54c25
Arthur Becker <arthur@chlorophyll-vision.com>
2013-06-19 22:36:33 +0200

    restructure server side

--- server.js
+++ server.js
@@ -8,8 +8,7 @@ var express = require('express')
 
 io.set('log level', 1);
 
-
-
+//Setting up server
 app.get('/', function (req, res) {
   res.sendfile('./public/views/index.html');
 });
@@ -23,22 +22,26 @@ app.configure(function(){
 
 server.listen(1337);
 
-//setting up game
-var gameLoopCallback = function(players) {
-  io.sockets.emit('update', {players:players, elapsedTime:36});
+logger.log('Server listening on port 1337', logger.cyan);
+
+//Setting up game
+var gameLoopCallback = function(players, elapsedTime) {
+  io.sockets.emit('update', {players:players, elapsedTime:elapsedTime});
 };
 
 game.addPlatform('top_floor', 0, 0, 1000, 32);
 game.addPlatform('right_floor', 968, 0, 32, 600);
 game.addPlatform('bottom_floor', 0, 600, 1000, 32);
 game.addPlatform('left_floor', 0, 0, 32, 600);
-
 game.addPlatform('middle_floor', 0, 135, 64, 32);
 
+game.start(gameLoopCallback);
+
+//Setting up socket IO
 io.sockets.on('connection', function (socket) {
     var address = socket.handshake.address;
     var id = address.address + ':' + address.port + '/' + curPlayerIndex++;
-    logger.log('New connection ' + id, logger.green);
+    logger.log('Connection : ' + id, logger.cyan);
     var player = game.addPlayer(id);
 
     //Give the platforms
@@ -48,7 +51,7 @@ io.sockets.on('connection', function (socket) {
 
     //Manage player disconnect
     socket.on('disconnect', function () {
-        logger.log('Player '+id + ' disconnect', logger.red);
+        logger.log('Disconnection : '+id, logger.cyan);
         game.removePlayer(id);
     });
 
@@ -64,5 +67,3 @@ io.sockets.on('connection', function (socket) {
       io.sockets.emit('chat_message', '>' + id + ' : ' + data);
     });
 });
-
-game.start(gameLoopCallback);
