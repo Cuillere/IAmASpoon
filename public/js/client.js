bbe3bc552a987808ce4e0db13e20cb6ea5a41238
Arthur Becker <arthur@chlorophyll-vision.com>
2013-06-19 22:23:39 +0200

    Restructure client side (separate js/html)

---
+++ public/js/client.js
@@ -0,0 +1,176 @@
+(function(){
+    "use strict"
+
+    //VARS
+    //--->Chat
+    var chat_box = document.getElementById("chat-box");
+    var chat_msg = document.getElementById("chat-message");
+    var chat_send = document.getElementById("chat-send");
+    //--->Graphics
+    var size = getWindowSize();
+    var assets = new Assets();
+    var canvas = document.getElementById("draw-space");
+    var ctx = canvas.getContext("2d");
+    var cursorX = 0, cursorY = 0;
+    //--->Graphics--->Sprites
+    var cursorSprite;
+    var playerSprite;
+    //--->Socket IO
+    var socket;
+    //--->Game data
+    var platforms;
+    var id;
+    var me;   //me.body me.id
+
+    //Setting up chat
+    chat_send.addEventListener('click', function() {
+        if(socket) {
+            var msg = chat_msg.value;
+            socket.emit('chat_message', msg);
+            chat_msg.value = '';
+        }
+    });
+
+    //Setting up canvas
+    canvas.width = size.w*2/3;
+    canvas.height = size.h*4/5;
+
+    canvas.addEventListener('mousemove', function(event) {
+        event.preventDefault();
+        cursorX = event.clientX - this.offsetLeft - cursorSprite.rect.width/2;
+        cursorY = event.clientY - this.offsetTop - cursorSprite.rect.height/2;
+    });
+
+    //Loading assets
+    assets.add("player","http://cdn.deguisetoi.fr/images/rep_articles/mini/ba/banane-a-etirer_213079_1.jpg");
+    assets.add("cursor","/images/cursor.jpg");
+    assets.load(function(progress, max){
+        ctx.fillRect(10,canvas.height/2,(canvas.width-20)*progress/max,3);
+    }, function(){
+        //Setting up sprites
+        var playerImages = [];
+        playerImages.push(assets.get('player'));
+        playerSprite = new Sprite(playerImages,32,32);
+        playerSprite.scaleType('deform');
+
+        var cursorImages = [];
+        cursorImages.push(assets.get('cursor'));
+        cursorSprite = new Sprite(cursorImages, 10, 10);
+        cursorSprite.scaleType('deform');
+    });
+
+
+
+    //Setting up Socket IO
+    //var socket = io.connect('http://109.217.85.232:1337');
+    socket = io.connect('http://192.168.1.12:1337');
+
+    socket.on('chat_message', function(data) {
+        var divTag = document.createElement("div");
+        divTag.innerHTML = data;
+        chat_box.appendChild(divTag);
+        chat_box.scrollTop = chat_box.scrollHeight;
+    });
+
+    socket.on('init_platforms', function(data) {
+        platforms = data;
+    });
+
+    socket.on('init_id', function(data) {
+        id = data;
+    });
+
+    socket.on('update', function(data) {
+        ctx.fillStyle = "rgb(50,90,155)";
+        ctx.fillRect(0,0,canvas.width,canvas.height);
+
+        //Find me
+        data.players.forEach(function(element, index, array) {
+            if(element.id == id) {
+                //This is me !
+                me = element;
+            }
+        });
+
+        var offsetX = canvas.width/2 - 16 -me.body.x;
+        var offsetY = canvas.height/2 - 16 -me.body.y;
+
+        if(playerSprite) {
+            data.players.forEach(function(element, index, array) {
+                playerSprite.move(element.body.x+offsetX, element.body.y+offsetY);
+                playerSprite.draw(ctx, data.elapsedTime);
+            });
+        }
+
+        platforms.forEach(function(element, index, array) {
+            ctx.fillStyle = "rgb(60,200,60)";
+            ctx.fillRect(element.body.x+offsetX, element.body.y+offsetY, element.body.width, element.body.height);
+        });
+
+        if(cursorSprite) {
+            cursorSprite.move(cursorX, cursorY);
+            cursorSprite.draw(ctx, data.elapsedTime);
+        }
+    });
+
+    //Setting up keyboard events
+    document.onkeydown = function(event) {
+        var winObj = checkEventObj(event);
+        var intKeyCode = winObj.keyCode;
+        if(isKeyUp(intKeyCode)) {
+            setKeyDown(intKeyCode);
+            manageKeyboardInput('down', intKeyCode, socket);
+        }
+    };
+
+    document.onkeyup = function(event) {
+        var winObj = checkEventObj(event);
+        var intKeyCode = winObj.keyCode;
+        if(isKeyDown(intKeyCode)) {
+            setKeyUp(intKeyCode);
+            manageKeyboardInput('up', intKeyCode, socket);
+        }
+    };
+
+    function manageKeyboardInput(type, intKeyCode, socket) {
+        var keyStr = '';
+        switch(intKeyCode) {
+            case KEY_S:
+            case KEY_DOWN:
+                keyStr = 'down';
+                break;
+            case KEY_Z:
+            case KEY_UP:
+                keyStr = 'up';
+                break;
+            case KEY_Q:
+            case KEY_LEFT:
+                keyStr = 'left';
+                break;
+            case KEY_D:
+            case KEY_RIGHT:
+                keyStr = 'right';
+                break;
+        }
+        socket.emit('input', {type:type, key:keyStr});
+    };
+
+    function getWindowSize() {
+        var myWidth = 0, myHeight = 0;
+        if( typeof( window.innerWidth ) == 'number' ) {
+            //Non-IE
+            myWidth = window.innerWidth;
+            myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+            //IE 6+ in 'standards compliant mode'
+            myWidth = document.documentElement.clientWidth;
+            myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+            //IE 4 compatible
+            myWidth = document.body.clientWidth;
+            myHeight = document.body.clientHeight;
+        }
+        return {w:myWidth, h:myHeight};
+    }
+
+})();
